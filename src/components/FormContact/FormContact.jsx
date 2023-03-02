a0c4947e301df2e9be37ab868d244f6dc7d86975
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-02 23:15:01 +0200

    Update FormContact.jsx

--- src/components/FormContact/FormContact.jsx
+++ src/components/FormContact/FormContact.jsx
@@ -50,7 +50,7 @@ const FormContact = () => {
         Number
         <Input
           type="tel"
-          name="number"
+          name="phone"
           value={phone}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
